a37bfc13d522ec1f26026c44750681ba7f6e5c40
Amr <amrmohamed.amaa@gmail.com>
2025-11-11 08:32:33 +0200

    Setup database

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  const users = await prisma.user.findMany({ include: { posts: true } });
+
   return (
     <div>
       <h1>Hello World</h1>
       <Button className='bg-red-500 text-white'>Test</Button>
+      <div className='grid place-content-center'>
+        <pre className='bg-neutral-100 p-4 rounded-md shadow-md border border-neutral-200'>
+          {JSON.stringify(users, null, 2)}
+        </pre>
+      </div>
     </div>
   );
 }
